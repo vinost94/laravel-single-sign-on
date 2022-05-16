6dc393fa44f6cfa0ac78571a50c5e1a2d0d9a504
Vinoth Premkumar <vino2420@gmail.com>
2022-05-16 14:58:50 +0530

    fixes route fixes

--- src/Routes/server.php
+++ src/Routes/server.php
@@ -5,8 +5,8 @@
  */
 
 Route::middleware('api')->prefix('api/sso')->group(function () {
-    Route::post('login', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController, 'login']);
-    Route::post('logout', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController, 'logout']);
-    Route::get('attach', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController, 'attach']);
-    Route::get('userInfo', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController, 'userInfo']);
+    Route::post('login', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController::class, 'login']);
+    Route::post('logout', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController::class, 'logout']);
+    Route::get('attach', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController::class, 'attach']);
+    Route::get('userInfo', [Vinothst94\LaravelSingleSignOn\Controllers\ServerController::class, 'userInfo']);
 });
